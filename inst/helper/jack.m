a9922ea54ab99ffedf8136a9d8d256253d576208
acp29 <acp29@sussex.ac.uk>
2022-03-11 23:19:10 +0000

    Update jack.m

--- inst/helper/jack.m
+++ inst/helper/jack.m
@@ -46,8 +46,13 @@ function [SE, T, U] = jack (x, func, paropt, opt)
   % Prepare data for resampling
   if nargin > 3
     if isfield(opt,'blocksize') && ~isempty(opt.blocksize)
-      % Block-jackknife resampling not yet supported
-      error('block resampling not implemented yet for jackknife')
+      % Prepare for Block-Jackknife
+      % Pad data (circular)
+      % Overlapping blocks of size l will be primary sampling unit
+      l = opt.blocksize;
+      for v = 1:nvar
+        x{v} = num2cell(cat(1,x{v},x{v}(1:l-1)));  % for circular blocks
+      end
     elseif isfield(opt,'clusters') && ~isempty(opt.clusters)
       % Prepare Cluster-Jackknife
       % Set whole clusters as primary sampling units
