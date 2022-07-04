06efef221306be2fa60dc385030967db232c2430
acp29 <acp29@sussex.ac.uk>
2022-07-05 00:53:04 +0100

    Update smoothmedian.cpp

--- src/smoothmedian.cpp
+++ src/smoothmedian.cpp
@@ -122,7 +122,7 @@ void mexFunction (int nlhs, mxArray* plhs[],
     double a, b, range, T, U, D, R, step, nwt;
     
     // Loop through the data and apply smoothing to the median
-    int MaxIter = 99;
+    int MaxIter = 199;
     for (int k = 0; k < n ; k++) {
 
         // Copy the next row/column of the data to temporary vector and sort it
