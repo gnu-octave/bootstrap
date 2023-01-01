1e8b28ec427321c4b03ac22168a67c272bdc9b03
acp29 <acp29@sussex.ac.uk>
2023-01-01 21:50:58 +0000

    Update cor.m

--- inst/param/cor.m
+++ inst/param/cor.m
@@ -1,6 +1,6 @@
 %  Function file: cor
 %
-%  RHO = cor (X, Y)
+%  RHO = cor (X, Y, TYPE)
 %
 %  If X and Y are column vectors, a scalar value is returned represnting the 
 %  correlation coefficient RHO. If X and Y are matrices, then RHO will be a
@@ -12,7 +12,7 @@
 % 
 %  cor (X, Y) = ...
 %
-%     mean ( (X - mean (X)) .* (Y - mean (Y)) ) ./ (std (X, 1) .* std (Y, 1))
+%   mean ( (X - mean (X)) .* (Y - mean (Y)) ) ./ (std (X, 1) .* std (Y, 1))
 %
 %  The syntax in this function code is known to be compatible with
 %  recent versions of Octave (v3.2.4 on Debian 6 Linux 2.6.32) and
@@ -37,21 +37,38 @@
 %  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-function RHO = cor (X, Y)
+function RHO = cor (X, Y, TYPE)
 
   % Evaluate input arguments
-  if (nargin ~= 2)
+  if ((nargin < 2) || (nargin > 3))
     error ('cor: Invalid number of input arguments')
   end
   if (nargout > 1)
     error ('cor: Invalid number of output arguments')
   end
-  if (~ all (size (X) == size(Y)))
+  sz = size (X); n = sz (1);
+  if (~ all (sz == size (Y)))
     error ('cor: X and Y must be the same size')
   end
-  if (numel (size (X)) > 2)
+  if (numel (sz) > 2)
     error ('cor: arrays of more than 2 dimensions are not supported')
   end
 
+  % If TYPE is Spearman, convert X and Y to ranks
+  if (nargin < 3)
+    TYPE = 'Pearson';
+  end
+  switch (lower (TYPE))
+  case {'spearman', 'spearmans'}
+    for i = 1:sz(2)
+      X(:,i) = tiedrank (X(:,i));
+      Y(:,i) = tiedrank (Y(:,i));
+    end
+  case {'pearson', 'pearsons'}
+    % Do nothing
+  end
+
   % Calculate correlation coefficient
-  RHO = mean ( (X - mean (X)) .* (Y - mean (Y)) ) ./ (std (X, 1) .* std (Y, 1));
+  XERR = X - mean (X);
+  YERR = Y - mean (Y);
+  RHO = sum ( XERR .* YERR ) ./ (sqrt (sum (XERR.^2)) .* sqrt (sum (YERR.^2) ));
