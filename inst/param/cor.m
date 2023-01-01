7c3a82f8e8354ee701f9e982db358952b28ad72b
acp29 <acp29@sussex.ac.uk>
2023-01-01 12:35:03 +0000

    Create cor.m

---
+++ inst/param/cor.m
@@ -0,0 +1,57 @@
+%  Function file: cor
+%
+%  RHO = cor (X, Y)
+%
+%  If X and Y are column vectors, a scalar value is returned represnting the 
+%  correlation coefficient RHO. If X and Y are matrices, then RHO will be a
+%  row vector corresponding to column-wise correlation coefficients. Hence this
+%  function is vectorised for rapid computation of the correlation coefficient
+%  in bootstrap resamples. Note that unlike the native @corr function, the
+%  correlation coefficients returned here are representative of the finite
+%  data sample and are not unbiased estimates of the population parameter.
+% 
+%  cor (X, Y) = ...
+%
+%     mean ( (X - mean (X)) .* (Y - mean (Y)) ) ./ (std (X, 1) .* std (Y, 1))
+%
+%  The syntax in this function code is known to be compatible with
+%  recent versions of Octave (v3.2.4 on Debian 6 Linux 2.6.32) and
+%  Matlab (v6.5.0 and v7.4.0 on Windows XP).
+%
+%  cor (version 2023.01.01)
+%  Author: Andrew Charles Penn
+%  https://www.researchgate.net/profile/Andrew_Penn/
+%
+%  Copyright 2019 Andrew Charles Penn
+%  This program is free software: you can redistribute it and/or modify
+%  it under the terms of the GNU General Public License as published by
+%  the Free Software Foundation, either version 3 of the License, or
+%  (at your option) any later version.
+%
+%  This program is distributed in the hope that it will be useful,
+%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%  GNU General Public License for more details.
+%
+%  You should have received a copy of the GNU General Public License
+%  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+function RHO = cor (X, Y)
+
+  % Evaluate input arguments
+  if (nargin ~= 2)
+    error ('cor: Invalid number of input arguments')
+  end
+  if (nargout > 1)
+    error ('cor: Invalid number of output arguments')
+  end
+  if (~ all (size (X) == size(Y)))
+    error ('cor: X and Y must be the same size')
+  end
+  if (numel (size (X)) > 2)
+    error ('cor: arrays of more than 2 dimensions are not supported')
+  end
+
+  % Calculate correlation coefficient
+  RHO = mean ( (X - mean (X)) .* (Y - mean (Y)) ) ./ (std (X, 1) .* std (Y, 1));
