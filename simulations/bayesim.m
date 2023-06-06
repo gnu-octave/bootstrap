43546cd7a27024c6188ff9180786bf641506cbc3
acp29 <acp29@sussex.ac.uk>
2023-06-06 15:07:30 +0100

    minor update to `bayesim`

--- simulations/bayesim.m
+++ simulations/bayesim.m
@@ -1,4 +1,4 @@
-% BAYESIAN CREDIBLE INTERVAL SIMULATION SCRIPT
+% BAYESIAN BOOTSTRAP CREDIBLE INTERVAL SIMULATION SCRIPT
 clear
 
 % bootbayes settings
@@ -6,19 +6,25 @@ nboot = 10000;
 prob = 0.95;
 
 % Simulation settings
-n = 7;
+n = 4;
 mu = 0;
 sigma = 1;
-nsamp = 1e+04;                  % Total sample size desired from simulation
+nsamp = 1e+05;                  % Total sample size desired from simulation
 nsim = 1e+07;                   % Size of simulation block
 eps = 5e-02 * sigma * sqrt (n); % Stringency/precision
 mu_good = [];
 
 % Create random sample
 y = randn (n, 1) * sigma + mu; 
-mu_within_range_of_y = (min(y) <= mu) & (mu <= max(y));
-range = max(y) - min(y);
-s = std (y, 1); % Standard deviation (sample as a population)
+mu_within_range_of_y = (min (y) <= mu) & (mu <= max (y));
+range = max (y) - min (y);
+s = std (y, 1);        % Standard deviation (2nd moment, sample as a population)
+if (exist ('pearsrnd'))
+  skewflag = true;
+  g = skewness (y, 1); % Skewness (3rd moment, sample as population)
+else
+  skewflag = false;
+end
 
 % Compute credible interval
 stats = bootbayes (y, [], [], nboot, prob);
@@ -32,11 +38,14 @@ fprintf ('Sample size: %u\n', n);
 fprintf ('nboot: %u\n', nboot);
 fprintf ('Prior: %.2f\n', 1.0);
 if (numel(prob) > 1)
-  fprintf (' Credible interval (CI) type: Percentile interval\n');;
-  fprintf (' Credible interval: %.3g%% (%.1f%%, %.1f%%)\n', mass, 100 * prob);
+  fprintf ('Credible interval (CI) type: Percentile interval\n');
+  mass =  100 * abs (prob(2) - prob(1));
+  fprintf ('Credible interval: %.3g%% (%.1f%%, %.1f%%)\n', ...
+           mass, 100 * prob);
 else
   fprintf ('Credible interval (CI) type: Shortest probability interval\n');
-  fprintf ('Credible interval: %.3g%%\n', 100 * prob);
+  mass = 100 * prob;
+  fprintf ('Credible interval: %.3g%%\n', mass);
 end
 fprintf ('Population mean within range of sample: %s\n', ...
          LogicalStr{mu_within_range_of_y + 1});
@@ -54,18 +63,21 @@ while (numel (mu_good) < nsamp)
   count = count + 1;
   fprintf (repmat ('\b', 1, 7));
   fprintf ('% 6s%%', sprintf ('%.1f', round (1000 * numel (mu_good) / nsamp) / 10));
-
   % Sample some mean values from the (flat) prior within 
   % the range of the observed data (non-parametric)
-  mu_sim = rand (1, nsim) * range + min(y);
+  mu_sim = rand (1, nsim) * range + min (y);
 
-  % Simulate data for each of these mean values
-  Y = randn (n, nsim) * s;
+  % Simulate data for each of these mean values with the same scale as the sample
+  if (skewflag)
+    Y = pearsrnd (0, s, g, 3, n, nsim); % 1st, 2nd and 3rd moment
+  else
+    Y = randn (n, nsim) * s;            % 1st and 2nd moment
+  end
   Y = bsxfun (@plus, Y, mu_sim);
 
   % Find simulated data that matches our "observed" data
-  Y = sort(Y);
-  y = sort(y);
+  Y = sort (Y);
+  y = sort (y);
   i = all (abs (bsxfun (@minus, Y, y)) <= eps);
   mu_good = cat (2, mu_good, mu_sim(i));
 
@@ -78,9 +90,11 @@ mu_good_within_CI = (CI(1) <= mu_good(1:nsamp)) & (mu_good(1:nsamp) <= CI(2));
 % Probability that population mean lies within the CI (Bayesian)'
 fprintf (repmat ('\b', 1, 7));
 fprintf ('% 6s%%', sprintf ('%.1f', 100));
-fprintf (['\nGiven our prior belief that the population mean could be equally\n',...
-          'likely within the range of the data, our updated belief (posterior)\n',...
-          'is summarized as a credible interval within which the population\n',...
-          'mean effectively exists with %.2f%% probability.\n'],...
-          sum (mu_good_within_CI) / nsamp * 100);
+fprintf (['\n Our prior belief is that the location of the population\n',...
+          ' mean is equally likely anywhere within the range of the data.\n',...
+          ' Our updated belief (i.e. posterior) after non-parametric\n',...
+          ' Bayesian bootstrap is summarized as a credible interval within\n',...
+          ' which the population mean exists with effectively %.2f%%\n',...
+          ' probability (nominally %.1f%%).\n'],...
+          sum (mu_good_within_CI) / nsamp * 100, mass);
 
