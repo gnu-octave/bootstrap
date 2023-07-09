316d0026580306fbe586486a9452bef5246f6dcf
acp29 <acp29@sussex.ac.uk>
2023-07-09 21:42:58 +0100

    Update bootlm.m

--- inst/bootlm.m
+++ inst/bootlm.m
@@ -729,8 +729,8 @@ function [STATS, X, L] = bootlm (Y, GROUP, varargin)
             case 'wild'
               STATS = bootwild (Y, X, DEP, NBOOT, ALPHA, SEED, L);
             case {'bayes', 'bayesian'}
-              wgt = bsxfun (@rdivide, n_dim(pairs), sum (n_dim(pairs)')');
-              prior = sum ((1 - wgt) .* (1 - 2 ./ n_dim(pairs)), 2);
+              wgt = bsxfun (@rdivide, n_dim(pairs')', sum (n_dim(pairs')', 2));
+              prior = sum ((1 - wgt) .* (1 - 2 ./ n_dim(pairs')'), 2);
               STATS = flatten_struct (cell2mat (arrayfun (@ (i) bootbayes ...
                         (Y, X, DEP, NBOOT, fliplr (1 - ALPHA), prior(i), SEED, ...
                         L(:, i)), (1:size (L, 2)), 'UniformOutput', false)));
