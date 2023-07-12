78c4ee2e261805d98e0212e72ffc49ceb95bb30d
acp29 <acp29@sussex.ac.uk>
2023-07-12 23:31:00 +0100

    Update bootci.m

--- inst/bootci.m
+++ inst/bootci.m
@@ -61,7 +61,7 @@
 %     calibrated percentile bootstrap confidence intervals CI, with the
 %     calibrated percentiles of the bootstrap statistics estimated from NBOOTCAL
 %     bootstrap data samples. NBOOTCAL is a positive integer value. The default
-%     value of NBOOTCAL is 200.
+%     value of NBOOTCAL is 199.
 %
 %     'CI = bootci (..., 'seed', SEED)' initialises the Mersenne Twister random
 %     number generator using an integer SEED value so that bootci results are
@@ -147,7 +147,7 @@ function [ci, bootstat, bootsam] = bootci (argin1, argin2, varargin)
   alpha = 0.05;
   type = 'bca';
   nbootstd = 100;
-  nbootcal = 200;
+  nbootcal = 199;
   paropt = struct;
   paropt.UseParallel = false;
   if (~ ISOCTAVE)
@@ -388,7 +388,7 @@ end
 %!         0 33 28 34 4 32 24 47 41 24 26 30 41].';
 %!
 %! ## 95% BCa bootstrap confidence intervals for the mean
-%! ci = bootci (2000, @mean, data)
+%! ci = bootci (1999, @mean, data)
 
 %!demo
 %!
@@ -397,7 +397,7 @@ end
 %!         0 33 28 34 4 32 24 47 41 24 26 30 41].';
 %!
 %! ## 95% calibrated percentile bootstrap confidence intervals for the mean
-%! ci = bootci (2000, {@mean, data}, 'type', 'cal','nbootcal',200)
+%! ci = bootci (1999, {@mean, data}, 'type', 'cal','nbootcal',199)
 %!
 %! ## Please be patient, the calculations will be completed soon...
 
@@ -409,7 +409,7 @@ end
 %!
 %! ## 95% calibrated percentile bootstrap confidence intervals for the median
 %! ## with smoothing
-%! ci = bootci (2000, {@smoothmedian, data}, 'type', 'cal', 'nbootcal', 200)
+%! ci = bootci (1999, {@smoothmedian, data}, 'type', 'cal', 'nbootcal', 199)
 %!
 %! ## Please be patient, the calculations will be completed soon...
 
@@ -420,7 +420,7 @@ end
 %!         0 33 28 34 4 32 24 47 41 24 26 30 41].';
 %!
 %! ## 90% percentile bootstrap confidence intervals for the variance
-%! ci = bootci (2000, {{@var,1}, data}, 'type', 'per', 'alpha', 0.1)
+%! ci = bootci (1999, {{@var,1}, data}, 'type', 'per', 'alpha', 0.1)
 
 %!demo
 %!
@@ -429,7 +429,7 @@ end
 %!         0 33 28 34 4 32 24 47 41 24 26 30 41].';
 %!
 %! ## 90% BCa bootstrap confidence intervals for the variance
-%! ci = bootci (2000, {{@var,1}, data}, 'type', 'bca', 'alpha', 0.1)
+%! ci = bootci (1999, {{@var,1}, data}, 'type', 'bca', 'alpha', 0.1)
 
 %!demo
 %!
@@ -438,7 +438,7 @@ end
 %!         0 33 28 34 4 32 24 47 41 24 26 30 41]';
 %!
 %! ## 90% Studentized bootstrap confidence intervals for the variance
-%! ci = bootci (2000, {{@var,1}, data}, 'type', 'stud', 'nbootstd', 50, 'alpha', 0.1)
+%! ci = bootci (1999, {{@var,1}, data}, 'type', 'stud', 'nbootstd', 50, 'alpha', 0.1)
 %!
 %! ## Please be patient, the calculations will be completed soon...
 
@@ -449,7 +449,7 @@ end
 %!         0 33 28 34 4 32 24 47 41 24 26 30 41].';
 %!
 %! ## 90% calibrated percentile bootstrap confidence intervals for the variance
-%! ci = bootci (2000, {{@var,1}, data}, 'type', 'cal', 'nbootcal', 200, 'alpha', 0.1)
+%! ci = bootci (1999, {{@var,1}, data}, 'type', 'cal', 'nbootcal', 199, 'alpha', 0.1)
 %!
 %! ## Please be patient, the calculations will be completed soon...
 
@@ -462,7 +462,7 @@ end
 %!       4.74,3.29,5.55,2.82,4.23,3.23,2.56,4.31,4.37,2.4].';
 %!
 %! ## 95% BCa bootstrap confidence intervals for the correlation coefficient
-%! ci = bootci (2000, @cor, x, y)
+%! ci = bootci (1999, @cor, x, y)
 %!
 %! ## Please be patient, the calculations will be completed soon...
 
@@ -481,10 +481,10 @@ end
 %! ##      0 33 28 34 4 32 24 47 41 24 26 30 41].';
 %! ## varfun = @(A) var(A, 1);
 %! ## rng('default'); % For reproducibility
-%! ## rng('default'); ci1 = bootci (20000,{varfun,A},'alpha',0.1,'type','norm');
-%! ## rng('default'); ci2 = bootci (20000,{varfun,A},'alpha',0.1,'type','per');
-%! ## rng('default'); ci4 = bootci (20000,{varfun,A},'alpha',0.1,'type','bca');
-%! ## rng('default'); ci5 = bootci (20000,{varfun,A},'alpha',0.1,'type','stud');
+%! ## rng('default'); ci1 = bootci (19999,{varfun,A},'alpha',0.1,'type','norm');
+%! ## rng('default'); ci2 = bootci (19999,{varfun,A},'alpha',0.1,'type','per');
+%! ## rng('default'); ci4 = bootci (19999,{varfun,A},'alpha',0.1,'type','bca');
+%! ## rng('default'); ci5 = bootci (19999,{varfun,A},'alpha',0.1,'type','stud');
 %! ##
 %! ## Summary of results from Matlab's 'Statistics and Machine Learning toolbox' (R2020b)
 %! ##
@@ -493,7 +493,7 @@ end
 %! ## ci1 - normal       |  108.9 |  247.4 |  138.5 |  1.21 |
 %! ## ci2 - percentile   |   97.6 |  235.8 |  138.2 |  0.87 |
 %! ## ci4 - BCa          |  114.9 |  260.5 |  145.6 |  1.57 |*
-%! ## ci5 - bootstrap-t  |   46.6 |  232.6 |  186.0 |  0.49 |** 
+%! ## ci5 - bootstrap-t  |   46.7 |  232.5 |  185.8 |  0.49 |** 
 %! ## -------------------|--------|--------|--------|-------|
 %! ## parametric - exact |  118.4 |  305.2 |  186.8 |  2.52 |
 %! ##
@@ -518,7 +518,7 @@ end
 %! ##                var.t <- sum (U^2 / (n * (n - 1)));
 %! ##                return ( c(t, var.t) ) };
 %! ## set.seed(1)
-%! ## var.boot <- boot (data=A, statistic=boot.fun, R=20000, sim='balanced')
+%! ## var.boot <- boot (data=A, statistic=boot.fun, R=19999, sim='balanced')
 %! ## ci1 <- boot.ci (var.boot, conf=0.90, type="norm")
 %! ## ci2 <- boot.ci (var.boot, conf=0.90, type="perc")
 %! ## ci3 <- boot.ci (var.boot, conf=0.90, type="basic")
@@ -526,20 +526,20 @@ end
 %! ## ci5 <- boot.ci (var.boot, conf=0.90, type="stud")
 %! ##
 %! ## library (bootstrap)  # Functions from Efron and Tibshirani (1993)
-%! ## set.seed(1); ci4a <- bcanon (A, 20000, var.fun, alpha=c(0.05,0.95))
-%! ## set.seed(1); ci5a <- boott (A, var.fun, nboott=20000, nbootsd=500, perc=c(.05,.95))
+%! ## set.seed(1); ci4a <- bcanon (A, 19999, var.fun, alpha=c(0.05,0.95))
+%! ## set.seed(1); ci5a <- boott (A, var.fun, nboott=19999, nbootsd=499, perc=c(.05,.95))
 %! ##
 %! ## Summary of results from 'boot' and 'bootstrap' packages in R
 %! ##
 %! ## method             |   0.05 |   0.95 | length | shape |  
 %! ## -------------------|--------|--------|--------|-------|
-%! ## ci1  - normal      |  109.4 |  246.8 |  137.4 |  1.21 |
-%! ## ci2  - percentile  |   97.8 |  235.6 |  137.8 |  0.87 |
-%! ## ci3  - basic       |  107.4 |  245.3 |  137.9 |  1.15 |
-%! ## ci4  - BCa         |  116.9 |  264.0 |  147.1 |  1.69 |
-%! ## ci4a - BCa         |  116.2 |  264.0 |  147.8 |  1.67 |
-%! ## ci5  - bootstrap-t |  111.8 |  291.2 |  179.4 |  2.01 |
-%! ## ci5a - bootstrap-t |  112.7 |  292.6 |  179.9 |  2.06 |
+%! ## ci1  - normal      |  109.6 |  246.7 |  137.1 |  1.22 |
+%! ## ci2  - percentile  |   97.9 |  234.8 |  136.9 |  0.86 |
+%! ## ci3  - basic       |  108.3 |  245.1 |  136.8 |  1.16 |
+%! ## ci4  - BCa         |  116.0 |  260.7 |  144.7 |  1.60 |
+%! ## ci4a - BCa         |  115.8 |  260.6 |  144.8 |  1.60 |
+%! ## ci5  - bootstrap-t |  112.0 |  291.8 |  179.8 |  2.02 |
+%! ## ci5a - bootstrap-t |  116.1 |  290.9 |  174.8 |  2.16 |
 %! ## -------------------|--------|--------|--------|-------|
 %! ## parametric - exact |  118.4 |  305.2 |  186.8 |  2.52 |
 %! 
@@ -547,30 +547,30 @@ end
 %! ##
 %! ## A = [48 36 20 29 42 42 20 42 22 41 45 14 6 ...
 %! ##      0 33 28 34 4 32 24 47 41 24 26 30 41].';
-%! ## ci1 = bootci (20000,{{@var,1},A},'alpha',0.1,'type','norm','seed',1);
-%! ## ci2 = bootci (20000,{{@var,1},A},'alpha',0.1,'type','per','seed',1);
-%! ## ci3 = bootci (20000,{{@var,1},A},'alpha',0.1,'type','basic','seed',1);
-%! ## ci4 = bootci (20000,{{@var,1},A},'alpha',0.1,'type','bca','seed',1);
-%! ## ci5 = bootci (20000,{{@var,1},A},'alpha',0.1,'type','stud','nbootstd',100,'seed',1);
-%! ## ci6 = bootci (20000,{{@var,1},A},'alpha',0.1,'type','cal','nbootcal',500,'seed',1);
+%! ## ci1 = bootci (19999,{{@var,1},A},'alpha',0.1,'type','norm','seed',1);
+%! ## ci2 = bootci (19999,{{@var,1},A},'alpha',0.1,'type','per','seed',1);
+%! ## ci3 = bootci (19999,{{@var,1},A},'alpha',0.1,'type','basic','seed',1);
+%! ## ci4 = bootci (19999,{{@var,1},A},'alpha',0.1,'type','bca','seed',1);
+%! ## ci5 = bootci (19999,{{@var,1},A},'alpha',0.1,'type','stud','nbootstd',100,'seed',1);
+%! ## ci6 = bootci (19999,{{@var,1},A},'alpha',0.1,'type','cal','nbootcal',499,'seed',1);
 %! ##
 %! ## Summary of results from 'statistics-bootstrap' package for Octave/Matlab
 %! ##
 %! ## method             |   0.05 |   0.95 | length | shape |  
 %! ## -------------------|--------|--------|--------|-------|
-%! ## ci1 - normal       |  108.2 |  248.5 |  140.3 |  1.22 |
-%! ## ci2 - percentile   |   96.6 |  236.7 |  140.1 |  0.87 |
-%! ## ci3 - basic        |  106.4 |  246.5 |  140.1 |  1.15 |
-%! ## ci4 - BCa          |  115.4 |  266.1 |  150.4 |  1.69 |
-%! ## ci5 - bootstrap-t  |  110.8 |  293.5 |  182.8 |  2.01 |
-%! ## ci6 - calibrated   |  113.4 |  297.0 |  183.6 |  2.16 |
+%! ## ci1 - normal       |  108.2 |  248.6 |  140.4 |  1.22 |
+%! ## ci2 - percentile   |   96.1 |  237.0 |  140.9 |  0.87 |
+%! ## ci3 - basic        |  106.0 |  246.9 |  140.9 |  1.15 |
+%! ## ci4 - BCa          |  115.9 |  264.6 |  148.7 |  1.67 |
+%! ## ci5 - bootstrap-t  |  111.0 |  297.6 |  186.6 |  2.08 |
+%! ## ci6 - calibrated   |  113.4 |  284.9 |  171.5 |  1.95 |
 %! ## -------------------|--------|--------|--------|-------|
 %! ## parametric - exact |  118.4 |  305.2 |  186.8 |  2.52 |
 %! ##
 %! ## Simulation results for constructing 90% confidence intervals for the
 %! ## variance of a population N(0,1) from 1000 random samples of size 26
 %! ## (analagous to the situation above). Simulation performed using the
-%! ## bootsim script with nboot of 2000.
+%! ## bootsim script with nboot of 1999.
 %! ##
 %! ## method               | coverage |  lower |  upper | length | shape |
 %! ## ---------------------|----------|--------|--------|--------|-------|
@@ -579,7 +579,7 @@ end
 %! ## basic                |    79.5% |   2.6% |  17.9% |   0.78 |  1.09 |
 %! ## BCa                  |    85.0% |   4.3% |  10.7% |   0.84 |  1.63 |
 %! ## bootstrap-t          |    90.4% |   3.5% |   6.1% |   1.02 |  2.14 |
-%! ## calibrated           |    90.7% |   5.1% |   4.2% |   1.13 |  2.73 |
+%! ## calibrated           |    90.3% |   4.5% |   5.2% |   0.99 |  2.21 |
 %! ## ---------------------|----------|--------|--------|--------|-------|
 %! ## parametric - exact   |    90.8% |   3.7% |   5.5% |   0.99 |  2.52 |
 %!
@@ -598,44 +598,44 @@ end
 %! warning ('off', 'bootknife:parallel')
 %! try
 %!   y = randn (20, 1); 
-%!   bootci (2000, 'mean', y);
-%!   bootci (2000, @mean, y);
-%!   bootci (2000, @mean, y, 'alpha', 0.1);
-%!   bootci (2000, {'mean', y}, 'alpha', 0.1);
-%!   bootci (2000, {@mean, y}, 'alpha', 0.1);
-%!   bootci (2000, {@mean, y}, 'alpha', 0.1, 'seed', 1);
-%!   bootci (2000, {@mean, y}, 'alpha', 0.1, 'type', 'norm');
-%!   bootci (2000, {@mean, y}, 'alpha', 0.1, 'type', 'per');
-%!   bootci (2000, {@mean, y}, 'alpha', 0.1, 'type', 'basic');
-%!   bootci (2000, {@mean, y}, 'alpha', 0.1, 'type', 'bca');
-%!   bootci (2000, {@mean, y}, 'alpha', 0.1, 'type', 'stud');
-%!   bootci (2000, {@mean, y}, 'alpha', 0.1, 'type', 'stud', 'nbootstd', 100);
-%!   bootci (2000, {@mean, y}, 'alpha', 0.1, 'type', 'cal');
-%!   bootci (2000, {@mean, y}, 'alpha', 0.1, 'type', 'cal', 'nbootcal', 200);
+%!   bootci (1999, 'mean', y);
+%!   bootci (1999, @mean, y);
+%!   bootci (1999, @mean, y, 'alpha', 0.1);
+%!   bootci (1999, {'mean', y}, 'alpha', 0.1);
+%!   bootci (1999, {@mean, y}, 'alpha', 0.1);
+%!   bootci (1999, {@mean, y}, 'alpha', 0.1, 'seed', 1);
+%!   bootci (1999, {@mean, y}, 'alpha', 0.1, 'type', 'norm');
+%!   bootci (1999, {@mean, y}, 'alpha', 0.1, 'type', 'per');
+%!   bootci (1999, {@mean, y}, 'alpha', 0.1, 'type', 'basic');
+%!   bootci (1999, {@mean, y}, 'alpha', 0.1, 'type', 'bca');
+%!   bootci (1999, {@mean, y}, 'alpha', 0.1, 'type', 'stud');
+%!   bootci (1999, {@mean, y}, 'alpha', 0.1, 'type', 'stud', 'nbootstd', 100);
+%!   bootci (1999, {@mean, y}, 'alpha', 0.1, 'type', 'cal');
+%!   bootci (1999, {@mean, y}, 'alpha', 0.1, 'type', 'cal', 'nbootcal', 199);
 %!   Y = randn (20); 
-%!   bootci (2000, 'mean', Y);
-%!   bootci (2000, @mean, Y);
-%!   bootci (2000, @mean, Y, 'alpha', 0.1);
-%!   bootci (2000, {'mean', Y}, 'alpha', 0.1);
-%!   bootci (2000, {@mean, Y}, 'alpha', 0.1);
-%!   bootci (2000, {@mean, Y}, 'alpha', 0.1, 'seed', 1);
-%!   bootci (2000, {@mean, Y}, 'alpha', 0.1, 'type', 'norm');
-%!   bootci (2000, {@mean, Y}, 'alpha', 0.1, 'type', 'per');
-%!   bootci (2000, {@mean, Y}, 'alpha', 0.1, 'type', 'basic');
-%!   bootci (2000, {@mean, Y}, 'alpha', 0.1, 'type', 'bca');
-%!   bootci (2000, {@mean, Y}, 'alpha', 0.1, 'type', 'stud');
-%!   bootci (2000, {@mean, Y}, 'alpha', 0.1, 'type', 'cal');
+%!   bootci (1999, 'mean', Y);
+%!   bootci (1999, @mean, Y);
+%!   bootci (1999, @mean, Y, 'alpha', 0.1);
+%!   bootci (1999, {'mean', Y}, 'alpha', 0.1);
+%!   bootci (1999, {@mean, Y}, 'alpha', 0.1);
+%!   bootci (1999, {@mean, Y}, 'alpha', 0.1, 'seed', 1);
+%!   bootci (1999, {@mean, Y}, 'alpha', 0.1, 'type', 'norm');
+%!   bootci (1999, {@mean, Y}, 'alpha', 0.1, 'type', 'per');
+%!   bootci (1999, {@mean, Y}, 'alpha', 0.1, 'type', 'basic');
+%!   bootci (1999, {@mean, Y}, 'alpha', 0.1, 'type', 'bca');
+%!   bootci (1999, {@mean, Y}, 'alpha', 0.1, 'type', 'stud');
+%!   bootci (1999, {@mean, Y}, 'alpha', 0.1, 'type', 'cal');
 %!   y = randn (20,1); x = randn (20,1); X = [ones(20,1),x];
-%!   bootci (2000, @cor, x, y);
-%!   bootci (2000, @(y,X) pinv(X)*y, y, X);
-%!   bootci (2000, @(y,X) pinv(X)*y, y, X, 'alpha', 0.1);
-%!   bootci (2000, {@(y,X) pinv(X)*y, y, X}, 'alpha', 0.1);
-%!   bootci (2000, {@(y,X) pinv(X)*y, y, X}, 'alpha', 0.1, 'type', 'norm');
-%!   bootci (2000, {@(y,X) pinv(X)*y, y, X}, 'alpha', 0.1, 'type', 'per');
-%!   bootci (2000, {@(y,X) pinv(X)*y, y, X}, 'alpha', 0.1, 'type', 'basic');
-%!   bootci (2000, {@(y,X) pinv(X)*y, y, X}, 'alpha', 0.1, 'type', 'bca');
-%!   bootci (2000, {@(y,X) pinv(X)*y, y, X}, 'alpha', 0.1, 'type', 'stud');
-%!   bootci (2000, {@(y,X) pinv(X)*y, y, X}, 'alpha', 0.1, 'type', 'cal');
+%!   bootci (1999, @cor, x, y);
+%!   bootci (1999, @(y,X) pinv(X)*y, y, X);
+%!   bootci (1999, @(y,X) pinv(X)*y, y, X, 'alpha', 0.1);
+%!   bootci (1999, {@(y,X) pinv(X)*y, y, X}, 'alpha', 0.1);
+%!   bootci (1999, {@(y,X) pinv(X)*y, y, X}, 'alpha', 0.1, 'type', 'norm');
+%!   bootci (1999, {@(y,X) pinv(X)*y, y, X}, 'alpha', 0.1, 'type', 'per');
+%!   bootci (1999, {@(y,X) pinv(X)*y, y, X}, 'alpha', 0.1, 'type', 'basic');
+%!   bootci (1999, {@(y,X) pinv(X)*y, y, X}, 'alpha', 0.1, 'type', 'bca');
+%!   bootci (1999, {@(y,X) pinv(X)*y, y, X}, 'alpha', 0.1, 'type', 'stud');
+%!   bootci (1999, {@(y,X) pinv(X)*y, y, X}, 'alpha', 0.1, 'type', 'cal');
 %! catch
 %!   warning ('on', 'bootknife:parallel')
 %!   rethrow (lasterror)
@@ -651,36 +651,36 @@ end
 %!
 %! ## Nonparametric 90% percentile confidence intervals (single bootstrap)
 %! ## Table 14.2 percentile intervals are 100.8 - 233.9
-%! ci = bootci(2000,{{@var,1},A},'alpha',0.1,'type','per','seed',1);
+%! ci = bootci(1999,{{@var,1},A},'alpha',0.1,'type','per','seed',1);
 %! if (isempty (regexp (which ('boot'), 'mex$')))
 %!   ## test boot m-file result
-%!   assert (ci(1), 95.24158837039771, 1e-07);
-%!   assert (ci(2), 237.7156378257705, 1e-07);
+%!   assert (ci(1), 95.33589724383222, 1e-07);
+%!   assert (ci(2), 237.1866652820803, 1e-07);
 %! end
 %!
 %! ## Nonparametric 90% BCa confidence intervals (single bootstrap)
 %! ## Table 14.2 BCa intervals are 115.8 - 259.6
-%! ci = bootci(2000,{{@var,1},A},'alpha',0.1,'type','bca','seed',1);
+%! ci = bootci(1999,{{@var,1},A},'alpha',0.1,'type','bca','seed',1);
 %! if (isempty (regexp (which ('boot'), 'mex$')))
 %!   ## test boot m-file result
-%!   assert (ci(1), 113.2388308884533, 1e-07);
-%!   assert (ci(2), 264.9901439787903, 1e-07);
+%!   assert (ci(1), 113.2858617321027, 1e-07);
+%!   assert (ci(2), 264.0328613673329, 1e-07);
 %! end
 %!
 %! ## Nonparametric 90% bootstrap-t confidence intervals (double bootstrap)
-%! ci = bootci(2000,{{@var,1},A},'alpha',0.1,'type','stud','nbootstd',100,'seed',1);
+%! ci = bootci(1999,{{@var,1},A},'alpha',0.1,'type','stud','nbootstd',100,'seed',1);
 %! if (isempty (regexp (which ('boot'), 'mex$')))
 %!   ## test boot m-file result
-%!   assert (ci(1), 109.3907006307414, 1e-07);
-%!   assert (ci(2), 306.8096054314711, 1e-07);
+%!   assert (ci(1), 109.3905360300909, 1e-07);
+%!   assert (ci(2), 303.9831191834784, 1e-07);
 %! end
 %!
 %! ## Nonparametric 90% calibrated percentile confidence intervals (double bootstrap)
-%! ci = bootci(2000,{{@var,1},A},'alpha',0.1,'type','cal','nbootcal',200,'seed',1);
+%! ci = bootci(1999,{{@var,1},A},'alpha',0.1,'type','cal','nbootcal',199,'seed',1);
 %! if (isempty (regexp (which ('boot'), 'mex$')))
 %!   ## test boot m-file result
-%!   assert (ci(1), 110.6138073406352, 1e-07);
-%!   assert (ci(2), 305.1908284023669, 1e-07);
+%!   assert (ci(1), 109.7569412835685, 1e-07);
+%!   assert (ci(2), 277.4147390170027, 1e-07);
 %! end
 %!
 %! ## Exact intervals based on normal theory are 118.4 - 305.2 (Table 14.2)
@@ -697,27 +697,27 @@ end
 %!
 %! ## Nonparametric 90% percentile confidence intervals (single bootstrap)
 %! ## Percentile intervals on page 266 are 0.524 - 0.928
-%! ci = bootci(2000,{@cor,LSAT,GPA},'alpha',0.1,'type','per','seed',1);
+%! ci = bootci(1999,{@cor,LSAT,GPA},'alpha',0.1,'type','per','seed',1);
 %! if (isempty (regexp (which ('boot'), 'mex$')))
 %!   ## test boot m-file result
-%!   assert (ci(1), 0.5056363801008388, 1e-07);
-%!   assert (ci(2), 0.9586254199016858, 1e-07);
+%!   assert (ci(1), 0.5046604076771914, 1e-07);
+%!   assert (ci(2), 0.9586661175519952, 1e-07);
 %! end
 %!
 %! ## Nonparametric 90% BCa confidence intervals (single bootstrap)
 %! ## BCa intervals on page 266 are 0.410 - 0.923
-%! ci = bootci(2000,{@cor,LSAT,GPA},'alpha',0.1,'type','bca','seed',1);
+%! ci = bootci(1999,{@cor,LSAT,GPA},'alpha',0.1,'type','bca','seed',1);
 %! if (isempty (regexp (which ('boot'), 'mex$')))
 %!   ## test boot m-file result
-%!   assert (ci(1), 0.4119228032301614, 1e-07);
-%!   assert (ci(2), 0.9300646701004258, 1e-07);
+%!   assert (ci(1), 0.4094344122396682, 1e-07);
+%!   assert (ci(2), 0.9301552819706758, 1e-07);
 %! end
 %!
 %! ## Nonparametric 90% calibrated percentile confidence intervals (double bootstrap)
-%! ci = bootci(2000,{@cor,LSAT,GPA},'alpha',0.1,'type','cal','nbootcal',500,'seed',1);
+%! ci = bootci(1999,{@cor,LSAT,GPA},'alpha',0.1,'type','cal','nbootcal',499,'seed',1);
 %! if (isempty (regexp (which ('boot'), 'mex$')))
 %!   ## test boot m-file result
-%!   assert (ci(1), 0.2307337185192847, 1e-07);
-%!   assert (ci(2), 0.9444347128107354, 1e-07);
+%!   assert (ci(1), 0.2078720903008157, 1e-07);
+%!   assert (ci(2), 0.9419996961228243, 1e-07);
 %! end
 %! ## Exact intervals based on normal theory are 0.51 - 0.91
