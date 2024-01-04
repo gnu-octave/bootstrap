1f6b7597ca843ff5d8b932e70955d7ff561e93ba
acp29 <acp29@sussex.ac.uk>
2024-01-04 10:59:25 +0000

    Moved manual to docs folder and publish page using GitHub pages

--- man/make_docs.m
+++ man/make_docs.m
@@ -1,5 +1,5 @@
 % THIS SCRIPT IS ONLY INTENDED FOR USE BY THE PACKAGE MAINTAINER
-% Run GNU Octave from a unix-based OS with pandoc installed. 
+% Run GNU Octave from a Bash terminal in a unix-based OS with pandoc installed. 
 % Octave needs to have the following packages installed:
 %  - generate_html
 %  - statistics
@@ -8,6 +8,7 @@
 
 % Remove existing pages and files
 system ('rm -Rf ../docs/*');
+system ('rm -Rf ../tmp');
 
 % Load required packages
 pkg load generate_html
@@ -20,16 +21,16 @@ myopt = get_html_options ("octave-forge");
 % Customize the options
 myopt.include_package_news = false;
 myopt.include_alpha = false;
-myopt.include_demos = false;
+myopt.include_demos = true;
 myopt.download_link = "https://gnu-octave.github.io/packages/statistics-resampling/";
 myopt.repository_link = "https://github.com/gnu-octave/statistics-resampling/";
 myopt.older_versions_download = "https://github.com/gnu-octave/statistics-resampling/releases";
 myopt.index_body_command = myopt.body_command;
 myopt.overview_filename = 'function_reference.html';
-generate_package_html ('statistics-resampling', 'manual_docs', myopt);
+generate_package_html ('statistics-resampling', 'tmp', myopt);
 
 % Run (bash) shell script
-system ('./generate_manual_helper.sh');
+system ('./make_docs_helper.sh');
 
 % Clear up
 clear myopt
---
+++ man/make_docs_helper.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Delete files that are surplus to requirements
+rm ./tmp/footer.js
+rm ./tmp/news.png
+rm ./tmp/manual.png
+rm ./tmp/oct.png
+rm ./tmp/favicon.ico
+
+# Move custom javascript and css documents to a subfolder in the dir containing index.html
+mkdir ./tmp/statistics-resampling/site-files
+cp ./templates/javascript.js ./tmp/statistics-resampling/site-files/javascript.js
+cp ./templates/manual.css ./tmp/statistics-resampling/site-files/manual.css
+cp ../doc/icon_48x48.png ./tmp/statistics-resampling/site-files/pkg_icon_48x48.png
+rm ./tmp/javascript.js
+rm ./tmp/octave-forge.css
+mv ./tmp/fixed.js ./tmp/statistics-resampling/site-files/fixed.js
+mv ./tmp/doc.png ./tmp/statistics-resampling/site-files/doc.png
+mv ./tmp/homepage.png ./tmp/statistics-resampling/site-files/homepage.png
+mv ./tmp/repository.png ./tmp/statistics-resampling/site-files/repository.png
+mv ./tmp/download.png ./tmp/statistics-resampling/site-files/download.png
+mv ./tmp/statistics-resampling/COPYING.html ./tmp/statistics-resampling/copying.html
+rm -Rf ./tmp/icons
+
+# Update file references
+sed -i '' -e  "s/write_top_menu (\(.*\));/write_top_menu ('\.');/g"  ./tmp/statistics-resampling/*.html
+sed -i '' -e  "s/write_top_menu (\(.*\));/write_top_menu ('\.\.');/g" ./tmp/statistics-resampling/function/*.html
+sed -i '' -e  "s/write_docs_left_menu (\(.*\));/write_docs_left_menu ('\.');/g" ./tmp/statistics-resampling/*.html
+sed -i '' -e  "s/write_docs_left_menu (\(.*\));/write_docs_left_menu ('\.\.');/g" ./tmp/statistics-resampling/function/*.html
+sed -i '' -e  's/\.\.\/octave-forge\.css/\.\/site-files\/manual\.css/g' ./tmp/statistics-resampling/*.html
+sed -i '' -e  's/\.\.\/octave-forge\.css/site-files\/manual\.css/g' ./tmp/statistics-resampling/function/*.html
+sed -i '' -e  's/\.\.\/fixed\.js/\.\/site-files\/fixed\.js/g' ./tmp/statistics-resampling/*.html
+sed -i '' -e  's/\.\.\/fixed\.js/site-files\/fixed\.js/g' ./tmp/statistics-resampling/function/*.html
+sed -i '' -e  's/\.\.\/javascript\.js/\.\/site-files\/javascript\.js/g' ./tmp/statistics-resampling/*.html
+sed -i '' -e  's/\.\.\/javascript\.js/site-files\/javascript\.js/g' ./tmp/statistics-resampling/function/*.html
+sed -i '' -e  '/footer\.js/d' ./tmp/statistics-resampling/*.html
+sed -i '' -e  '/footer\.js/d' ./tmp/statistics-resampling/function/*.html
+sed -i '' -e  's/<script> write_footer (); <\/script>//g' ./tmp/statistics-resampling/*.html
+sed -i '' -e  's/<script> write_footer (); <\/script>//g' ./tmp/statistics-resampling/function/*.html
+sed -i '' -e  's/\.\.\/download\.png/\.\/site-files\/download\.png/g' ./tmp/statistics-resampling/index.html
+sed -i '' -e  's/\.\.\/repository\.png/\.\/site-files\/repository\.png/g' ./tmp/statistics-resampling/index.html
+sed -i '' -e  's/\.\.\/doc\.png/\.\/site-files\/doc\.png/g' ./tmp/statistics-resampling/index.html
+sed -i '' -e  's/\.\.\/homepage\.png/\.\/site-files\/homepage\.png/g' ./tmp/statistics-resampling/index.html
+sed -i '' -e  's/COPYING\.html/copying\.html/g' ./tmp/statistics-resampling/index.html
+
+# Change page headings of the index (package info) and function reference pages
+sed -i '' -e  's/statistics-resampling<\/h2>/About this package<\/h2>/g' ./tmp/statistics-resampling/index.html
+sed -i '' -e  's/statistics-resampling<\/h2>/Function Reference<\/h2>/g' ./tmp/statistics-resampling/function_reference.html
+
+# Publish README markdown file as HTML and add header
+cd ..
+pandoc README.md -o ./man/tmp/statistics-resampling/tmp.html
+cat ./man/templates/header.html ./man/tmp/statistics-resampling/tmp.html > ./man/tmp/statistics-resampling/readme.html
+printf "</div>\n</body>\n</html>\n" >> ./man/tmp/statistics-resampling/readme.html
+rm ./man/tmp/statistics-resampling/tmp.html
+cd man
+
+# Add hyperlink to function reference directly in the HTML of the readme file (since the link in the markdown file would fail on the GitHub page) 
+#sed -i '' -e  's/Function Reference/\<a href\=\"function_reference\.html\"\>Function Reference\<\/a\>/g' ./tmp/statistics-resampling/readme.html
+
+# Update meta information in all html files
+sed -i '' -e  's/<meta name="author" content="\(.*\)" \/>/<meta name="author" content="Andrew Penn" \/>/' ./tmp/statistics-resampling/*.html
+sed -i '' -e  's/<meta name="author" content="\(.*\)" \/>/<meta name="author" content="Andrew Penn" \/>/' ./tmp/statistics-resampling/function/*.html
+sed -i '' -e  's/<title>\(.*\)<\/title>/<title>The statistics-resampling package manual<\/title>/' ./tmp/statistics-resampling/*.html
+sed -i '' -e  's/<title>\(.*\)<\/title>/<title>The statistics-resampling package manual<\/title>/' ./tmp/statistics-resampling/function/*.html
+sed -i '' -e  's/<meta name="description" content="\(.*\)" \/>/<meta name="description" content="A package for statistical analysis using resampling methods." \/>/' ./tmp/statistics-resampling/*.html
+sed -i '' -e  's/<meta name="description" content="\(.*\)" \/>/<meta name="description" content="A package for statistical analysis using resampling methods." \/>/' ./tmp/statistics-resampling/function/*.html
+sed -i '' -e  's/<meta name="keywords" lang="en" content="\(.*\)" \/>/<meta name="keywords" lang="en" content="GNU Octave Packages, MATLAB Toolbox\" \/>/' ./tmp/statistics-resampling/*.html
+sed -i '' -e  's/<meta name="keywords" lang="en" content="\(.*\)" \/>/<meta name="keywords" lang="en" content="GNU Octave Packages, MATLAB Toolbox\" \/>/' ./tmp/statistics-resampling/function/*.html
+sed -i '' -e  '/<link rel="shortcut icon" href="\.\.\/favicon.ico" \/>/d' ./tmp/statistics-resampling/*.html
+sed -i '' -e  '/<link rel="shortcut icon" href="\.\.\/.\.\/favicon.ico" \/>/d' ./tmp/statistics-resampling/function/*.html
+
+# Fixes for validation of XHTML 1.0 Strict
+sed -i '' -e  's/<script>/<script type="text\/javascript">/g' ./tmp/statistics-resampling/*.html
+sed -i '' -e  's/<script>/<script type="text\/javascript">/g' ./tmp/statistics-resampling/function/*.html
+sed -i '' -e  's/alt="Repository icon">/alt="Repository icon"\/>/' ./tmp/statistics-resampling/index.html
+
+# Copy web pages and files to docs folder and clean up
+cp -R ./tmp/statistics-resampling/* ../docs/
+rm -Rf ./tmp
