651b027909e98c2e615f0845c2a43f29dbabc20d
acp29 <A.C.Penn@sussex.ac.uk>
2024-04-02 00:21:45 +0100

    Update post_install.m

--- post_install.m
+++ post_install.m
@@ -1,6 +1,6 @@
 function post_install (d)
 
-  fprintf ('The statistics-resampling package was installed into the directory\n');
+  fprintf ('The statistics-resampling package was installed into the directory:\n');
   fprintf ('%s.\n', d.dir);
   fprintf ('License and copyright information can be found in ');
   fprintf ('\"packinfo/COPYING\"\n');
